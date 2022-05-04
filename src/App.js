a7f7922e68e2e98851438a62961444fbe3c74c2d
Toan <toanlpfx16432@funix.edu.vn>
2022-05-04 19:49:07 +0700

    Initialize project and add staff.jsx to src/shared

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
+
+class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+    };
+  }
+
+  render() {
+    return 
+  }
+}
+
+export default App;
