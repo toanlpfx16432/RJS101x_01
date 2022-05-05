52741224cf2b4e1dba72c036751b0cce365320cf
Toan <toanlpfx16432@funix.edu.vn>
2022-05-05 11:10:59 +0700

    import dữ liệu vào App và truyền STAFFS vào component StaffList

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponent';
-import { DISHES } from './shared/dishes';
+import STAFF from './shared/staffs';
+import StaffList from './components/StafflistComponent';
 
 class App extends Component {
 
   constructor(props){
     super(props);
 
-    this.state = {
-    };
+    this.state = {staff: STAFF};
   }
 
   render() {
-    return 
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className='container'>
+            <NavbarBrand href="/">Ứng dụng quản lý nhân sự v1.0</NavbarBrand>
+          </div>
+        </Navbar>
+        <StaffList staff = {this.state.staff}/>
+      </div>
+    )
   }
 }
 
---
+++ src/components/StafflistComponent.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+
+class StaffList extends Component {
+    constructor(pros) {
+        super(pros)
+        this.state={}
+    }
+}
+
+render () {
+    return (<div></div>)
+}
+
+export default StaffList
