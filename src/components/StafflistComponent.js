83eb3bccd18cf710574bdfe83ad0045161b2c025
Toan <toanlpfx16432@funix.edu.vn>
2022-05-05 16:20:58 +0700

    cài đặt dataFormat định dạng ngày tháng theo mẫu

--- src/components/StafflistComponent.js
+++ src/components/StafflistComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import dateFormat from "dateformat";
 
 class StaffList extends Component {
     constructor(pros) {
@@ -23,8 +24,8 @@ class StaffList extends Component {
                     <CardImg width="100%" src={staff.image} alt={staff.name} />
                     <CardBody>
                         <CardTitle>Họ và tên: {staff.name}</CardTitle>
-                        <CardText>Ngày sinh: {staff.doB}</CardText>
-                        <CardText>Ngày vào công ty: {staff.startDate}</CardText>
+                        <CardText>Ngày sinh: {dateFormat(staff.doB, 'dd/mm/yyyy')}</CardText>
+                        <CardText>Ngày vào công ty: {dateFormat(staff.startDate, "dd/mm/yyyy")}</CardText>
                         <CardText>Phòng ban: {staff.department.name}</CardText>
                         <CardText>Số ngày nghỉ còn lại: {staff.annualLeave}</CardText>
                         <CardText>Số ngày đã làm thêm: {staff.overTime}</CardText>
