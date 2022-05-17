943ece6239ad3d41d1a992ec37422a540c30e382
Toan <toanlpfx16432@funix.edu.vn>
2022-05-17 16:04:36 +0700

    create Header Footer

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -6,7 +6,18 @@ function Header() {
         <div>
             <Navbar dark color="primary">
                 <div className='container'>
-                    <NavbarBrand href="/">Ứng dụng quản lý nhân sự v1.0</NavbarBrand>
+                    <NavbarBrand><img src='assets/images/logo.png' width="40" alt='Quan Ly Nhan Vien'/></NavbarBrand>
+                    <Nav>
+                        <NavItem>
+                            <span className='fa fa-users fa-lg'></span> Nhân Viên
+                        </NavItem>
+                        <NavItem>
+                            <span className='fa fa-users fa-lg'></span> Phòng Ban
+                        </NavItem>
+                        <NavItem>
+                            <span className='fa fa-users fa-lg'></span> Bảng Lương
+                        </NavItem>
+                    </Nav>
                 </div>
             </Navbar>
         </div>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {Navbar, NavbarBrand, Nav, NavbarToggler, NavItem, Collapse} from 'reactstrap';
+import Footer from './FooterComponent';
 import Header from './HeaderComponent';
 
 function Main() {
     return (
         <div>
             <Header />
+            <Footer />
         </div>
     )
 }
