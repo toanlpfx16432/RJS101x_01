b9cd821fc7290d8634b6b07b4434c787eeca65a6
Toan <toanlpfx16432@funix.edu.vn>
2022-05-23 10:43:05 +0700

    intro to Redux

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,46 +1,51 @@
-import React from 'react';
-import {Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem} from 'reactstrap';
-import { Link } from 'react-router-dom';
+import React from "react";
+import {
+  Card,
+  CardImg,
+  CardImgOverlay,
+  CardTitle,
+  Breadcrumb,
+  BreadcrumbItem,
+} from "reactstrap";
+import { Link } from "react-router-dom";
 
-    function RenderMenuItem ({ dish, onClick}) {
-        return (
-            <Card>
-                <Link to={`/menu/${dish.id}`} >
-                    <CardImg src={dish.image} alt={dish.name} />
-                    <CardImgOverlay>
-                        <CardTitle>{dish.name}</CardTitle>
-                    </CardImgOverlay>
-                </Link>
-            </Card>   
-        );
-    }
+function RenderMenuItem({ dish, onClick }) {
+  return (
+    <Card>
+      <Link to={`/menu/${dish.id}`}>
+        <CardImg src={dish.image} alt={dish.name} />
+        <CardImgOverlay>
+          <CardTitle>{dish.name}</CardTitle>
+        </CardImgOverlay>
+      </Link>
+    </Card>
+  );
+}
 
-    const Menu = (props) => {
-        const menu = props.dishes.map((dish)=> {
-            return(
-                    <div key={dish.id} className='col-12 col-md-5 m-1'>
-                        <RenderMenuItem dish={dish}/>
-                    </div>
-            );
-        });
-        return (
-            <div className='container'>
-                <div className='row'>
-                    <Breadcrumb>
-                        <BreadcrumbItem><Link to='/home'>Home</Link></BreadcrumbItem>
-                        <BreadcrumbItem active>Menu</BreadcrumbItem>
-                    </Breadcrumb>
-                    <div className='col-12'>
-                        <h3>Menu</h3>
-                        <hr />
-                    </div>
-                </div>
-                <div className='row'>
-                    {menu}
-                </div>
-            </div>
-        );
-    }
-export default Menu
-        
-    
+const Menu = (props) => {
+  const menu = props.dishes.map((dish) => {
+    return (
+      <div key={dish.id} className="col-12 col-md-5 m-1">
+        <RenderMenuItem dish={dish} />
+      </div>
+    );
+  });
+  return (
+    <div className="container">
+      <div className="row">
+        <Breadcrumb>
+          <BreadcrumbItem>
+            <Link to="/home">Home</Link>
+          </BreadcrumbItem>
+          <BreadcrumbItem active>Menu</BreadcrumbItem>
+        </Breadcrumb>
+        <div className="col-12">
+          <h3>Menu</h3>
+          <hr />
+        </div>
+      </div>
+      <div className="row">{menu}</div>
+    </div>
+  );
+};
+export default Menu;
