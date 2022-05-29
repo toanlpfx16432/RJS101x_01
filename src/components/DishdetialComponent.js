3420be261f87ff807398d60494f71d3476af31d4
Toan <toanlpfx16432@funix.edu.vn>
2022-05-29 15:53:53 +0700

    Redux Thunk

--- src/components/DishdetialComponent.js
+++ src/components/DishdetialComponent.js
@@ -11,6 +11,7 @@ import {
 } from "reactstrap";
 import { Link } from "react-router-dom";
 import { Control, LocalForm, Errors } from "react-redux-form";
+import { Loading } from "./LoadingComponents";
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !val || val.length <= len;
@@ -147,7 +148,25 @@ class CommentForm extends Component {
 }
 
 const DishDetail = (props) => {
-  if (props.dish != null) {
+  if (props.isLoading) {
+    return(
+      <div className="container">
+        <div className="row">
+          <Loading />
+        </div>
+      </div>
+    )
+  }
+  else if (props.errMess) {
+    return(
+      <div className="container">
+        <div className="row">
+          <h4>{props.errMess}</h4>
+        </div>
+      </div>
+    )
+  }
+  else if (props.dish != null) {
     return (
       <div className="container">
         <div className="row">
--- src/redux/actiontypes.js
+++ src/redux/actiontypes.js
@@ -1,1 +1,4 @@
 export const ADD_COMMENT = 'ADD_COMMENT';
+export const DISHES_LOADING = 'DISHES_LOADING';
+export const DISHES_FAIED = 'DISHES_FAIED';
+export const ADD_DISHES ='ADD_DISHES';
