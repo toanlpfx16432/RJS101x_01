dab9b1eeb947a2d48054cb385845848ed7171624
Toan <toanlpfx16432@funix.edu.vn>
2022-05-31 18:29:19 +0700

    LAB12_1

--- src/components/DishdetialComponent.js
+++ src/components/DishdetialComponent.js
@@ -13,7 +13,6 @@ import { Link } from "react-router-dom";
 import { Control, LocalForm, Errors } from "react-redux-form";
 import { Loading } from "./LoadingComponents";
 import { baseUrl} from '../shared/baseUrl';
-import { FadeTransform, Fade, Stagger} from 'react-animation-components';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !val || val.length <= len;
@@ -21,20 +20,13 @@ const minLength = (len) => (val) => val && val.length >= len;
 
 function RenderDish({ dish }) {
   return (
-    <div className="col-12 col-md-5 m-1">
-      <FadeTransform in 
-        transformProps={{
-          exitTransform: 'scale(0.5) translateY(-50%)'
-        }}>
-        <Card key={dish.id}>
-          <CardImg top src={baseUrl + dish.image} alt={dish.name} />
-          <CardBody>
-            <CardTitle>{dish.name}</CardTitle>
-            <CardText>{dish.description}</CardText>
-          </CardBody>
-        </Card>
-      </FadeTransform>
-    </div>
+    <Card key={dish.id} className="col-12 col-md-5 m-1">
+      <CardImg top src={baseUrl + dish.image} alt={dish.name} />
+      <CardBody>
+        <CardTitle>{dish.name}</CardTitle>
+        <CardText>{dish.description}</CardText>
+      </CardBody>
+    </Card>
   );
 }
 function RenderComments({ comments, postComment, dishId }) {
@@ -44,25 +36,21 @@ function RenderComments({ comments, postComment, dishId }) {
       <div className="col-12 col-md-5 m-1">
         <h4>Comments</h4>
         <ul className="list-unstyled">
-          <Stagger in>
           {comments.map((comment) => {
             return (
-              <Fade key={comment.id} in>
-                <li>
-                  <p>{comment.comment}</p>
-                  <p>
-                    -- {comment.author},{" "}
-                    {new Intl.DateTimeFormat("en-US", {
-                      year: "numeric",
-                      month: "short",
-                      day: "2-digit",
-                    }).format(new Date(Date.parse(comment.date)))}
-                  </p>
-                </li>
-              </Fade>
+              <li key={comment.id}>
+                <p>{comment.comment}</p>
+                <p>
+                  -- {comment.author},{" "}
+                  {new Intl.DateTimeFormat("en-US", {
+                    year: "numeric",
+                    month: "short",
+                    day: "2-digit",
+                  }).format(new Date(Date.parse(comment.date)))}
+                </p>
+              </li>
             );
           })}
-          </Stagger>
         </ul>
         <CommentForm dishId={dishId} postComment={postComment}/>
       </div>
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -9,7 +9,6 @@ import {
 } from "reactstrap";
 import { Loading } from './LoadingComponents';
 import { baseUrl} from '../shared/baseUrl'
-import { FadeTransform } from 'react-animation-components';
 
 function RenderCard({ item, isLoading, errMess }) {
   if (isLoading) {
@@ -24,21 +23,16 @@ function RenderCard({ item, isLoading, errMess }) {
   }
   else
     return (
-      <FadeTransform in 
-        transformProps={{
-          exitTransform: 'scale(0.5) translateY(-50%)'
-        }}>
-        <Card>
-          <CardImg src={baseUrl + item.image} alt={item.name} />
-          <CardBody>
-            <CardTitle>{item.name}</CardTitle>
-            {item.designation ? (
-              <CardSubtitle>{item.designation}</CardSubtitle>
-            ) : null}
-            <CardText>{item.description}</CardText>
-          </CardBody>
-        </Card>
-      </FadeTransform>
+      <Card>
+        <CardImg src={baseUrl + item.image} alt={item.name} />
+        <CardBody>
+          <CardTitle>{item.name}</CardTitle>
+          {item.designation ? (
+            <CardSubtitle>{item.designation}</CardSubtitle>
+          ) : null}
+          <CardText>{item.description}</CardText>
+        </CardBody>
+      </Card>
     );
 }
 
